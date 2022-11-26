e67a5983786c6c66ca6ebfc24bb8a7cc7b012993
0anw0 <ahmadnabilwanas@gmail.com>
2022-11-26 04:31:51 +0000

    configuring Android & Adding TypeScript tsconfig to Project

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return(
+    <View style={{flex:1, justifyContent: 'center', alignItems: 'center',}}>
+      <Text style={{fontSize:16, fontWeight:'700', color: 'red'}}> App </Text>
+    </View>
+  )
+}
