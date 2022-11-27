11a89338f3f9d4dc820fa13981dee5ab1f84cb87
0anw0 <ahmadnabilwanas@gmail.com>
2022-11-27 06:54:10 +0200

    Adding Super Heros BIO

--- scr/utils/herosdata.tsx
+++ scr/utils/herosdata.tsx
@@ -11,7 +11,7 @@ export const HerosData: HerosDataType[] = [
       imageUri +
       'd/d7/CapAmerica-EndgameProfile.jpg/revision/latest/scale-to-width-down/1000?cb=20190423175339',
     description:
-      'In the present day, scientists in the Arctic uncover an old, frozen aircraft. In March 1942, Nazi lieutenant general Johann Schmidt and his men steal a mysterious relic called the Tesseract,[N 2] which possesses untold godly powers, from the town of Tønsberg in German-occupied Norway.',
+      `Steve Rogers was a would-be U.S. Army enlistee rejected by recruiters because of his small size. He volunteers to receive a top-secret serum and transforms into a “super-soldier.” Dubbed Captain America and clad in a red, white, and blue costume with a matching stars-and-stripes shield, Rogers joins the army.`,
   },
   {
     id: 1,
@@ -20,7 +20,7 @@ export const HerosData: HerosDataType[] = [
       imageUri +
       '3/35/IronMan-EndgameProfile.jpg/revision/latest?cb=20190423175213',
     description:
-      'In the present day, scientists in the Arctic uncover an old, frozen aircraft. In March 1942, Nazi lieutenant general Johann Schmidt and his men steal a mysterious relic called the Tesseract,[N 2] which possesses untold godly powers, from the town of Tønsberg in German-occupied Norway.',
+      `Tony Stark is the wealthy son of industrialist and weapons manufacturer Howard Stark and his wife, Maria. Tony grew up a genius with a brilliant mind for technology and inventions and, naturally, followed in his father's footsteps, inheriting Stark Industries upon his parents' untimely death.`,
   },
   {
     id: 2,
@@ -28,7 +28,7 @@ export const HerosData: HerosDataType[] = [
     uri:
       imageUri +
       '2/22/Thor_in_LoveAndThunder_Poster.jpg/revision/latest?cb=20220615195810',
-    description: '',
+    description: `Thor is the son of Odin, the All-Father and King of Asgard. He was worshiped by the humans known as Vikings and became part of Norse Mythology. Armed with his enchanted Uru hammer Mjolnir, which helps him to channel his godly energies, he became the mightiest warrior in the Ten Realms`,
   },
   {
     id: 3,
@@ -36,7 +36,7 @@ export const HerosData: HerosDataType[] = [
     uri:
       imageUri +
       '7/79/Hulk_in_the_She-Hulk_series_-_Infobox.jpg/revision/latest?cb=20220819171053',
-    description: `It was patently apparent that [the monstrous character the] Thing was the most popular character in [Marvel's recently created superhero team the] Fantastic Four. ... For a long time, I'd been aware of the fact that people were more likely to favor someone who was less than perfect. ... It's a safe bet that you remember Quasimodo, but how easily can you name any of the heroic, handsomer, more glamorous characters in The Hunchback of Notre Dame? And then there's Frankenstein.`,
+    description: `Dr. Bruce Banner lives a life caught between the soft-spoken scientist he's always been and the uncontrollable green monster powered by his rage. Exposed to heavy doses of gamma radiation, scientist Bruce Banner transforms into the mean, green rage machine called the Hulk.`,
   },
   {
     id: 4,
@@ -45,6 +45,6 @@ export const HerosData: HerosDataType[] = [
       imageUri +
       '9/9a/BlackWidow-EndgameProfile.jpg/revision/latest?cb=20190423174842',
     description:
-      'In the present day, scientists in the Arctic uncover an old, frozen aircraft. In March 1942, Nazi lieutenant general Johann Schmidt and his men steal a mysterious relic called the Tesseract,[N 2] which possesses untold godly powers, from the town of Tønsberg in German-occupied Norway.',
+     `Natasha was born in Stalingrad (now Volgograd), Russian SFSR, USSR. The first and best-known Black Widow is a Russian agent trained as a spy, martial artist, and sniper, and outfitted with an arsenal of high-tech weaponry, including a pair of wrist-mounted energy weapons dubbed her "Widow's Bite".`,
   },
 ];
